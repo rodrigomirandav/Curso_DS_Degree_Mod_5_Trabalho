1d01e7b1711005cea2b7e6b25056f28cafc769c7
Rodrigo de Miranda Videira <diguimirandajf@gmail.com>
2022-02-19 12:44:14 -0300

    Criando a importação bibliotecas

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+import pandas as pd
+import numpy as np
+
+# Gráficos
+import matplotlib.pyplot as plt
+import searborn as sns
+import plotly.express as px
+
+#Data prep
+import unidecode
+from dataprep.clean import clean_country
+
+def main():
+    pass
+
+
+if __name__ == 'main':
+    main()
