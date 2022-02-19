1dc43c8ed96724c2c3e320f810a2545839e1e4fd
Rodrigo de Miranda Videira <diguimirandajf@gmail.com>
2022-02-19 17:23:45 -0300

    Criando menus, alguns gráficos

--- main.py
+++ main.py
@@ -8,22 +8,78 @@ import unidecode
 from PIL import Image
 
 # from dataprep.clean import clean_country
-container_id = "id_principal"
+todos_streamings = pd.DataFrame()
+tipos_graficos = ["Pyplot", "Seaborn"]
+
+if 'tipo_grafico' not in st.session_state:
+    st.session_state.tipo_grafico = "Pyplot"
+
+if 'modulo_sistema' not in st.session_state:
+    st.session_state.modulo_sistema = 'principal'
+
+if 'quantidade_1' not in st.session_state:
+    st.session_state.quantidade_1 = 15
+
+
+def load_df_streamings():
+    amazon = pd.read_csv('https://letscodeeleicao.s3.us-west-1.amazonaws.com/amazon_prime_titles.csv')
+    netflix = pd.read_csv('https://letscodeeleicao.s3.us-west-1.amazonaws.com/netflix_titles.csv')
+    disney = pd.read_csv('https://letscodeeleicao.s3.us-west-1.amazonaws.com/disney_plus_titles.csv')
+
+    amazon.drop(['show_id'], axis=1, inplace=True)
+    netflix.drop(['show_id'], axis=1, inplace=True)
+    disney.drop(['show_id'], axis=1, inplace=True)
+
+    global todos_streamings
+    todos_streamings = pd.concat([netflix, amazon, disney])
+    todos_streamings.cast.fillna('', inplace=True)
+    todos_streamings.director.fillna('', inplace=True)
+    todos_streamings.listed_in.fillna('', inplace=True)
+    todos_streamings.country.fillna('', inplace=True)
+
+    todos_streamings.title = todos_streamings.title.apply(lambda title: unidecode.unidecode(title))
+    todos_streamings.director = todos_streamings.director.apply(lambda director: unidecode.unidecode(director))
+    todos_streamings.cast = todos_streamings.cast.apply(lambda cast: unidecode.unidecode(cast))
+    todos_streamings.listed_in = todos_streamings.listed_in.apply(lambda listed_in: unidecode.unidecode(listed_in))
+    todos_streamings.country = todos_streamings.country.apply(lambda country: unidecode.unidecode(country))
+
+
+def create_filter(container):
+    if st.session_state.modulo_sistema == 'analise_1':
+        container.subheader("Filtros")
+
+        container.selectbox('Tipos de gráficos',
+                            options=tipos_graficos,
+                            key='tipo_grafico')
+
+        container.slider('Quantidade de generos',
+                         min_value=5,
+                         max_value=15,
+                         key='quantidade_1')
 
 
 def create_sidebar():
     st.sidebar.title("Menu")
-    st.sidebar.button("Principal", key="id_principal", on_click=principal)
-    st.sidebar.button("Equipe", key="id_equipe", on_click=equipe)
-    st.sidebar.button("Equipe", key="id_equipe1")
+
+    st.sidebar.button("Principal",
+                      key="id_principal",
+                      on_click=change_module,
+                      args=('principal',))
+
+    st.sidebar.button("Equipe",
+                      key="id_equipe",
+                      on_click=change_module,
+                      args=('equipe',))
+
+    st.sidebar.button("1. Qual genero mais apareceu no geral",
+                      key="id_genero",
+                      on_click=change_module,
+                      args=('analise_1',))
+
     st.sidebar.button("Equipe", key="id_equipe2")
     st.sidebar.button("Equipe", key="id_equipe3")
     st.sidebar.button("Equipe", key="id_equipe4")
     st.sidebar.button("Equipe", key="id_equipe5")
-
-
-def main():
-    create_sidebar()
 
 
 def principal():
@@ -37,7 +93,9 @@ def principal():
         st.text("""
             * Utilizamos três bases de dados, extraídas do site Kagle
             \n* São bases dos três principais serviços de streamings (Nettflix, Amazon Prime \ne Disney+)
-            \n* Através das três bases coletadas utilizamos várias técnicas de \nKDD (Knowledge Discovery in Databases - Extração de conhecimento)\n para criar uma base única, e que foi utilizada para nossas análises
+            \n* Através das três bases coletadas utilizamos várias técnicas de 
+            \nKDD (Knowledge Discovery in Databases - Extração de conhecimento)
+            \n para criar uma base única, e que foi utilizada para nossas análises
         """)
 
 
@@ -51,5 +109,59 @@ def equipe():
         st.image(image, caption="Equipe")
 
 
-if __name__ == '__main__':
+def analise_1():
+    with st.container():
+        global todos_streamings
+        st.empty()
+        st.header("1. Qual genero mais apareceu no geral")
+
+        create_filter(st)
+
+        generos = todos_streamings[["title", "listed_in"]].copy()
+        generos = generos.listed_in.str.split(",").explode().fillna("Outros").str.strip()
+        generos = generos.value_counts().reset_index()
+        generos.columns = ["Gênero", "Quantidade de filmes"]
+        generos = generos[:15]
+        generos = generos.head(st.session_state.quantidade_1)
+
+        st.subheader("Dataframe")
+        st.dataframe(generos)
+
+        st.subheader("Gráfico")
+
+        if st.session_state.tipo_grafico == "Pyplot":
+            fig = px.bar(generos,
+                         x="Quantidade de filmes",
+                         y="Gênero",
+                         orientation='h',
+                         title='Quantidade de filmes por gênero')
+            fig.update_yaxes(autorange="reversed")
+            st.plotly_chart(fig)
+        elif st.session_state.tipo_grafico == "Seaborn":
+            fig, ax = plt.subplots()
+            ax = sns.barplot(data=generos, x='Gênero', y='Quantidade de filmes', palette="flare")
+            ax.set_xticklabels(ax.get_xticklabels(), rotation=60)
+
+            st.pyplot(fig)
+
+
+def change_module(module='principal'):
+    st.session_state.modulo_sistema = module
+
+
+def main():
+    create_sidebar()
+
+    if len(todos_streamings) == 0:
+        load_df_streamings()
+
+    if st.session_state.modulo_sistema == 'principal':
+        principal()
+    elif st.session_state.modulo_sistema == 'equipe':
+        equipe()
+    elif st.session_state.modulo_sistema == 'analise_1':
+        analise_1()
+
+
+if __name__ == "__main__":
     main()
