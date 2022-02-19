21624a8530119d59c104955088e5a6a3f441f03b
Rodrigo de Miranda Videira <diguimirandajf@gmail.com>
2022-02-19 13:20:43 -0300

    Alterando códigos errados

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import searborn as sns
+import seaborn as sns
 import plotly.express as px
 import unidecode
 from dataprep.clean import clean_country
 
 
 def main():
-    pass
+    st.sidebar.write("Teste")
 
 
 if __name__ == 'main':
