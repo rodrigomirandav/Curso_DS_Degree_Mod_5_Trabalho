88a66cc080ba18ead66d8e3f1cdeccb7f823cac2
Rodrigo de Miranda Videira <diguimirandajf@gmail.com>
2022-02-19 13:02:30 -0300

    Alterando nome arquivo requirements

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import streamlit as st
-
 import pandas as pd
 import numpy as np
-
-# Gráficos
 import matplotlib.pyplot as plt
 import searborn as sns
 import plotly.express as px
-
-#Data prep
 import unidecode
 from dataprep.clean import clean_country
+
 
 def main():
     pass
